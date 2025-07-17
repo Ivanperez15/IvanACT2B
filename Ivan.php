bea60dbded977bb08ab84dd8561453761ccf256f
Ivanperez15 <johnivan.angeles.15@gmail.com>
2025-07-17 08:30:37 +0800

    Add files via upload

---
+++ Ivan.php
@@ -0,0 +1,11 @@
+<?php 
+echo "My name is: John Ivan Perez Angeles <br>";
+echo "I am 18 <br>";
+echo "My favorite subject is: Webpage<br>";
+echo "My dream is: Web Developer<br>";
+echo "Kung kaya ng iba ipagawa mo sakanila! <br>";
+echo "
+    /\_/\ <br>";
+echo "( o.o )<br>";
+echo " > ^ <";
+?>
